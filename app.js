6ba7b7ed60ff7ddf18c0cb5fe0574447b3fdf9a1
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2016-11-14 18:33:04 +0300

    added object keys to strings

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const apiKey = '2f49b9f8b3fc474888e9f02575e4cdd6';
 const url = `https://newsapi.org/v1/articles?source=bbc-news&apiKey=${apiKey}`;
 
-var newsHeader = document.querySelector('header');
-var newsList = document.querySelector('ul');
+const newsHeader = document.querySelector('header');
+const newsList = document.querySelector('ul');
 
 fetch(url)
   .then(response => response.json()
@@ -12,13 +12,17 @@ fetch(url)
       newsHeaderText.innerHTML = json.source.toUpperCase();
       newsHeader.appendChild(newsHeaderText);
       
+      //const data = {} = json; //it destructures json
+
       for (i = 0; i < json.articles.length; i++) {
           
-          var listArticle = document.createElement('li');
+          let listArticle = document.createElement('li');
 
-          for (var prop in json.articles[i]) {
+          //let artKeys = Object.keys(json.articles[i]); //it gives me an array of keys 
+
+          for (let prop in json.articles[i]) {
             var newDiv = document.createElement('div');
-            newDiv.innerHTML = json.articles[i][prop];
+            newDiv.innerHTML = `${prop}: ` + json.articles[i][prop];
             listArticle.appendChild(newDiv); 
           }
           newsList.appendChild(listArticle);
