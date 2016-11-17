211b1b8b98fd6fbaeb347b9c829b5cc94395cf87
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2016-11-17 11:40:53 +0300

    added forEach method instead of for loop

--- app.js
+++ app.js
@@ -6,39 +6,34 @@ const newsHeader = document.querySelector('header');
 const newsList = document.querySelector('ul');
 
 fetch(url)
-  .then(response => response.json()
-    .then (json => {
+  .then(response => response.json())
+  .then (json => {
       newsHeaderText = document.createElement('h1');
       newsHeaderText.innerHTML = json.source.toUpperCase();
       newsHeader.appendChild(newsHeaderText);
 
-      for (i = 0; i < json.articles.length; i++) {
-          
-        let listArticle = document.createElement('li');
-        
-        let article = {} = json.articles[i];
-          //let artKeys = Object.keys(article); 
-          // artKeys.forEach(addElementToList(item, article[item], listArticle));                 
-        for (let key in article) {
-          addElementToList(key, article[key], listArticle);         
-        }   
-        addLinkToList(article.url, listArticle);
-
-        newsList.appendChild(listArticle);
-      }
+      json.articles.forEach(article => {
+        const listArticle = document.createElement('li');
+        const artKeys = Object.keys(article);
+       
+       artKeys.forEach(key => {
+         addElementToList(key, article[key], listArticle);
+       });
+       addLinkToList(article.url, listArticle);
+       newsList.appendChild(listArticle);
+     });
     })  
-  )
   .catch(error => console.log(error));
 
 
-var addElementToList = (x, y, list) => {
-  let newDiv = document.createElement('div');
+function addElementToList(x, y, list) {
+  const newDiv = document.createElement('div');
   newDiv.innerHTML = x + ": " + y;
   list.appendChild(newDiv);
 };
 
-var addLinkToList = (x, list) => {
-  let newLink = document.createElement('a');
+function addLinkToList(x, list) {
+  const newLink = document.createElement('a');
   newLink.setAttribute('href', x);
   newLink.innerHTML = 'Read more...';
   list.appendChild(newLink);
