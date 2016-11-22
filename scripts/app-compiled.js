3cf6dbdf33b8882af8d5407f601a9e21bb8f9900
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2016-11-22 19:26:33 +0300

    object methods

--- scripts/app-compiled.js
+++ scripts/app-compiled.js
@@ -7,72 +7,65 @@ var urlLink = 'http://newsapi.org';
 var newsHeader = document.querySelector('header');
 var newsList = document.querySelector('ul');
 
-fetch(url).then(function (response) {
-  return response.json();
-}).then(function (json) {
-
-  var news = new newsDAO(json);
-
-  news.addHeaderSource(newsHeader);
-
-  var newsSourceText = 'News powered by: News API';
-  news.addLinkToList(urlLink, newsHeader, newsSourceText);
-
-  json.articles.forEach(function (article) {
-    var listArticle = document.createElement('li');
-    var artKeys = Object.keys(article);
-
-    artKeys.slice(0, 3).forEach(function (key) {
-      return news.addElementToList(article[key], listArticle);
-    });
-
-    var alt = article.title;
-    var urlToImg = article.urlToImage;
-    news.addImageToList(urlToImg, alt, listArticle);
-
-    artKeys.slice(3).forEach(function (key) {
-      return news.addElementToList(article[key], listArticle);
-    });
-
-    var readMore = 'Read more...';
-    news.addLinkToList(article.url, listArticle, readMore);
-
-    newsList.appendChild(listArticle);
-  });
-}).catch(function (error) {
-  return console.log(error);
-});
-
-function newsDAO(json) {
-
-  this.json = json;
-
-  this.addHeaderSource = function (header) {
+var newsDAO = {
+  addHeaderSource: function addHeaderSource(source, header) {
     var newsHeaderText = document.createElement('h1');
-    newsHeaderText.innerHTML = json.source.toUpperCase();
+    newsHeaderText.innerHTML = source.toUpperCase();
     header.appendChild(newsHeaderText);
-  };
-
-  this.addLinkToList = function (x, list, text) {
+  },
+  addLinkToList: function addLinkToList(x, list, text) {
     var newLink = document.createElement('a');
     newLink.setAttribute('class', 'links');
     newLink.setAttribute('href', x);
     newLink.innerHTML = text;
     list.appendChild(newLink);
-  };
-
-  this.addElementToList = function (x, list) {
+  },
+  addElementToList: function addElementToList(x, list) {
     var newDiv = document.createElement('div');
     newDiv.innerHTML = x;
     list.appendChild(newDiv);
-  };
-
-  this.addImageToList = function (image, alt, list) {
+  },
+  addImageToList: function addImageToList(image, alt, list) {
     var newImage = document.createElement('img');
     newImage.setAttribute('src', image);
     newImage.setAttribute('alt', alt);
     newImage.setAttribute('class', 'imgClass');
     newImage.innerHTML;
     list.appendChild(newImage);
-  };
+  }
 };
+
+fetch(url).then(function (response) {
+  return response.json();
+}).then(function (json) {
+
+  var headerSource = json.source;
+  newsDAO.addHeaderSource(headerSource, newsHeader);
+
+  var newsSourceText = 'News powered by: News API';
+  newsDAO.addLinkToList(urlLink, newsHeader, newsSourceText);
+
+  json.articles.forEach(function (article) {
+    var listArticle = document.createElement('li');
+    var artKeys = Object.keys(article);
+
+    artKeys.slice(0, 3).forEach(function (key) {
+      return newsDAO.addElementToList(article[key], listArticle);
+    });
+
+    var alt = article.title;
+    var urlToImg = article.urlToImage;
+    newsDAO.addImageToList(urlToImg, alt, listArticle);
+
+    artKeys.slice(3).forEach(function (key) {
+      return newsDAO.addElementToList(article[key], listArticle);
+    });
+
+    var readMore = 'Read more...';
+    newsDAO.addLinkToList(article.url, listArticle, readMore);
+
+    newsList.appendChild(listArticle);
+  });
+}).catch(function (error) {
+  return console.log(error);
+});
--- scripts/app.js
+++ scripts/app.js
@@ -6,31 +6,59 @@ const urlLink = 'http://newsapi.org';
 const newsHeader = document.querySelector('header');
 const newsList = document.querySelector('ul');
 
+const newsDAO =  {
+  addHeaderSource: function(source, header) {
+    const newsHeaderText = document.createElement('h1');
+    newsHeaderText.innerHTML = source.toUpperCase();
+    header.appendChild(newsHeaderText);
+  },
+  addLinkToList: function(x, list, text) {
+    const newLink = document.createElement('a');
+    newLink.setAttribute('class', 'links');
+    newLink.setAttribute('href', x);
+    newLink.innerHTML = text;
+    list.appendChild(newLink);
+  },
+  addElementToList: function(x, list) {
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = x;
+    list.appendChild(newDiv);
+  },
+  addImageToList: function(image, alt, list) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    newImage.setAttribute('alt', alt);
+    newImage.setAttribute('class', 'imgClass');
+    newImage.innerHTML;
+    list.appendChild(newImage);
+  }
+};
+
+
 fetch(url)
   .then(response => response.json())
   .then (json => {
-
-    const news = new newsDAO(json);
     
-    news.addHeaderSource(newsHeader);
+    const headerSource = json.source;
+    newsDAO.addHeaderSource(headerSource, newsHeader);
 
     const newsSourceText = 'News powered by: News API';
-    news.addLinkToList(urlLink, newsHeader, newsSourceText);
+    newsDAO.addLinkToList(urlLink, newsHeader, newsSourceText);
 
     json.articles.forEach(article => {
       const listArticle = document.createElement('li');
       const artKeys = Object.keys(article);
       
-      artKeys.slice(0, 3).forEach(key => news.addElementToList(article[key], listArticle));
+      artKeys.slice(0, 3).forEach(key => newsDAO.addElementToList(article[key], listArticle));
 
       const alt = article.title;
       const urlToImg = article.urlToImage;
-      news.addImageToList(urlToImg, alt, listArticle);
+      newsDAO.addImageToList(urlToImg, alt, listArticle);
 
-      artKeys.slice(3).forEach(key => news.addElementToList(article[key], listArticle));
+      artKeys.slice(3).forEach(key => newsDAO.addElementToList(article[key], listArticle));
 
       const readMore = 'Read more...';        
-      news.addLinkToList(article.url, listArticle, readMore);   
+      newsDAO.addLinkToList(article.url, listArticle, readMore);   
       
       newsList.appendChild(listArticle);
     });
@@ -38,37 +66,4 @@ fetch(url)
   .catch(error => console.log(error));
 
 
-function newsDAO(json) {
-
-  this.json = json;
-
-  this.addHeaderSource = function(header) {
-    const newsHeaderText = document.createElement('h1');
-    newsHeaderText.innerHTML = json.source.toUpperCase();
-    header.appendChild(newsHeaderText);
-  };
-
-  this.addLinkToList = function(x, list, text) {
-    const newLink = document.createElement('a');
-    newLink.setAttribute('class', 'links');
-    newLink.setAttribute('href', x);
-    newLink.innerHTML = text;
-    list.appendChild(newLink);
-  };
-
-  this.addElementToList = function(x, list) {
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML = x;
-    list.appendChild(newDiv);
-  };
-
-  this.addImageToList = function(image, alt, list) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', image);
-    newImage.setAttribute('alt', alt);
-    newImage.setAttribute('class', 'imgClass');
-    newImage.innerHTML;
-    list.appendChild(newImage);
-  };
-};
 
